4011b9762560c63cea3c1a803fe64526891654e8
Marin Minnerly <me@vocksel.com>
2024-02-15 10:18:10 -0800

    Move mocks top `tests/mocks`

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 mod types;
 
 fn main() {
-    let petstore_spec = Path::new("src/mocks/commented-schema.yml");
+    let petstore_spec = Path::new("tests/mocks/commented-schema.yml");
     let spec = oas3::from_path(petstore_spec).expect("Failed to load the OpenAPI spec file");
 
     let tree = types::generate_luau_types(spec);
